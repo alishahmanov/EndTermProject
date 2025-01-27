d0f53c5cdc48871429e99143b43be9d5010f5f9f
Iskander <Assa>
2025-01-27 11:01:48 +0500

    Controllers commit

---
+++ src/main/java/controllers/ShoeController.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import models.Shoes;
+import controllers.interfaces.IShoeController;
+import repositories.interfaces.IShoesRepository;
+
+import java.util.List;
+
+public class ShoeController implements IShoeController {
+    private final IShoesRepository repo;
+
+    public ShoeController(IShoesRepository repo) {
+        this.repo = repo;
+    }
+
+    public String addShoe(String brand, String material, String color, String seasonOfShoes, boolean gender, int size, int price, String availability) {
+        if (price <= 0) {
+            return "Price must be greater than 0!";
+        }
+
+        if (size <= 0) {
+            return "Size must be greater than 0!";
+        }
+
+        Shoes shoe = new Shoes(gender, brand, material, seasonOfShoes, color, size, price, availability);
+        boolean created = repo.addShoe(shoe);
+
+        return (created ? "Success" : "Fail");
+    }
+
+    public String getAllShoes() {
+        List<Shoes> shoes = repo.getAllShoes();
+
+        if (shoes.isEmpty()) {
+            return "No sneakers found!";
+        }
+
+        StringBuilder response = new StringBuilder();
+        for (Shoes shoe : shoes) {
+            response.append(shoe).append("\n");
+        }
+
+        return response.toString();
+    }
+
+    public String getShoe(int id) {
+        Shoes shoe = repo.getShoe(id);
+
+        return (shoe == null ? "Sneaker was not found!" : shoe.toString());
+    }
+}
---
+++ src/main/java/controllers/interfaces/IShoeController.java
@@ -0,0 +1,7 @@
+package controllers.interfaces;
+
+public interface IShoeController {
+    String addShoe(String brand, String material, String color, String seasonOfShoes, boolean gender, int size, int price, String availability);
+    String getAllShoes();
+    String getShoe(int id);
+}
